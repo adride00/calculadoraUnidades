1dbc12a2ea3bb8703038729bd58db004fc3858e3
liliam-avilez <75285722+liliam-avilez@users.noreply.github.com>
2023-05-28 22:12:01 -0600

    Agregar tiempo al menu
    Se agrego las unidades de tiempo en el menu

---
+++ assets/constants/constants/menu.js
@@ -0,0 +1,27 @@
+export const MENUITEMS = [
+    {
+      nombre: 'Datos',
+      icono: 'data-storage-svgrepo-com.svg',
+    },
+    {
+      nombre: 'Moneda',
+      icono: 'money-mouth-face-svgrepo-com.svg',
+    },
+    {
+      nombre: 'Longitud',
+      icono: 'cm-inch-length-svgrepo-com.svg',
+    },
+    {
+      nombre: 'Peso',
+      icono: 'weight-svgrepo-com.svg',
+    },
+    {
+      nombre: 'Volumen',
+      icono: 'measuring-cup-svgrepo-com.svg',
+    },
+    {
+      nombre: 'Tiempo',
+      icono: 'time-svgrepo-com.svg',
+    }
+
+  ]
---
+++ assets/constants/constants/unidades.js
@@ -0,0 +1,21 @@
+export const UNIDADES = [
+  {
+    Peso: ["Gramos", "Kilogramos", "Libras", "Onzas", "Arrobas"],
+  },
+  {
+    Longitud: ["Metros", "Kilometros", "Millas", "Pies", "Centimetros"],
+  },
+  {
+    Volumen: ["Litros", "Mililitros", "Galones", "Onzas", "Botellas"],
+  },
+  {
+    Moneda: ["Dolares", "Euros", "Pesos", "Yen", "Won"],
+  },
+  {
+    Datos: ["Bits", "Bytes", "Kilobytes", "Megabytes", "Gigabytes"],
+  },
+  {
+    Tiempo: ["Segundos", "Minutos", "Horas", "Dias", "Semanas"]
+  }
+
+];
