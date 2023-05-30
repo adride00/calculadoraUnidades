54c5c15482fab109e34ef66e2a99f3ce5a5c5bc9
adride00 <adride@outlook.com>
2023-05-30 17:49:54 -0600

    💄 hotfix: se corrigio estilos de los botones (#8)

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,7 +5,7 @@ export const Options = () => {
      
     ${MENUITEMS.map((item) => `
       <div class="col-md-1">
-        <button id="${item.nombre}" type="button" class="btn btn-outline-light buttonType active" style="background-color: #007bff; color: #fff;"><img src="./assets/icons/${item.icono}" width="40px"/>${item.nombre}</button>
+        <button id="${item.nombre}" type="button" class="btn btn-outline-light buttonType"><img src="./assets/icons/${item.icono}" width="40px"/>${item.nombre}</button>
       </div>
     `).join('')}
   
