2d573550070c62ad00ddc9148857e75237e759ba
Karla C <125685537+lejazzcat@users.noreply.github.com>
2023-05-30 19:25:16 -0600

    Update Moneda.php
    tildes removidas

--- src/Classes/Moneda.php
+++ src/Classes/Moneda.php
@@ -2,7 +2,7 @@
 
 class Moneda extends UnitConverter {
   private $tabla = [
-    'Dólares' => [
+    'Dolares' => [
       'Euros' => 0.93,
       'Pesos Mexicanos' => 17.63,
       'Yenes' => 140.59,
@@ -10,28 +10,28 @@ class Moneda extends UnitConverter {
     ],
 
     'Euros' => [
-      'Dólares' => 1.97,
+      'Dolares' => 1.97,
       'Pesos Mexicanos' => 18.91,
       'Yenes' => 150.75,
       'Wones' => 1419.34
     ],
 
     'Pesos Mexicanos' => [
-      'Dólares' => 0.057,
+      'Dolares' => 0.057,
       'Euros' => 0.053,
       'Yenes' => 7.98,
       'Wones' => 75.07
     ],
 
     'Yenes' => [
-      'Dólares' => 0.0071,
+      'Dolares' => 0.0071,
       'Euros' => 0.0066,
       'Pesos Mexicanos' => 0.13,
       'Wones' => 9.41
     ],
 
     'Wones' => [
-      'Dólares' => 0.00076,
+      'Dolares' => 0.00076,
       'Euros' => 0.00070,
       'Pesos Mexicanos' => 0.013,
       'Yenes' => 0.11
