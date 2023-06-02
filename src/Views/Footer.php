def380830114fb8f73432c6d990a80ef55dd5610
Karla C <125685537+lejazzcat@users.noreply.github.com>
2023-06-01 19:47:46 -0600

    dani and kc collab (#16)
    * Add files via upload
    
    Added icons
    
    * Update style.css
    
    footer styles added
    
    * Update Footer.php
    
    Added footer

---
+++ src/Views/Footer.php
@@ -0,0 +1,83 @@
+<?php
+<footer class="footer">
+      <ul class="content-members">
+        <li class="nav-item">
+          <a
+            href="https://github.com/adride00"
+            class="nav-link px-2 text-body-secondary">
+            <img class="img" src="./iconos/ADRIAN.svg" alt="Adrian" />
+            <p>Adrián</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a
+            href="https://github.com/4Rurik"
+            class="nav-link px-2 text-body-secondary">
+            <img class="img" src="./iconos/RO.svg" alt="Ro" />
+            <p>Rodrigo</p>
+          </a>
+        </li>
+
+        
+        <li class="nav-item">
+          <a
+            href="https://github.com/liliam-avilez"
+            class="nav-link px-2 text-body-secondary"
+            ><img class="img" src="./iconos/LILI.svg" alt="Lily" />
+            <p>
+              Liliam
+            </p>
+            </a>
+        </li>
+
+        
+        <li class="nav-item">
+          <a
+            href="https://github.com/lejazzcat"
+            class="nav-link px-2 text-body-secondary"
+            ><img class="img" src="./iconos/Karla.svg" alt="Karla" />
+            <p>Karla
+            </p>
+          </a>
+        </li>
+
+        
+        <li class="nav-item">
+          <a
+            href="https://github.com/LexD19"
+            class="nav-link px-2 text-body-secondary"
+            ><img class="img" src="./iconos/VLADY.svg" alt="Valdy" />
+            <p>
+              Vladimir
+            </p>
+          </a>
+        </li>
+
+        
+        <li class="nav-item">
+          <a
+            href="https://github.com/MajoEra"
+            class="nav-link px-2 text-body-secondary"
+            ><img class="img" src="./iconos/MAJO.svg" alt="Majo" />
+            <p>
+              María José
+            </p>
+          </a>
+        </li>
+
+        
+        <li class="nav-item">
+          <a
+            href="https://github.com/azuort"
+            class="nav-link px-2 text-body-secondary"
+            ><img class="img" src="./iconos/AZU.svg" alt="Dany" />
+            <p>
+              Daniela
+            </p>
+          </a>
+        </li>
+      </ul>
+      <p class="text-center text-body-secondary">© 2023</p>
+    </footer>
+  ?>
